177566a6a7c3b10346c6cf65e53af4b46602999e
fangniu <838307912@qq.com>
2017-04-06 09:39:12 +0800

    add hosts

--- hosts/models.py
+++ hosts/models.py
@@ -7,8 +7,18 @@ __author__ = 'Sheng Chen'
 
 
 class Host(models.Model):
-    processor = models.SmallIntegerField(u'逻辑核心数')
-    memory = models.IntegerField(u'内存(MB)')
+    kernel = models.CharField(u'系统内核', max_length=32)
+    cpu_model = models.CharField(u'CPU型号', max_length=255)
+    num_cpus = models.SmallIntegerField(u'CPU核数')
+    cpuarch = models.CharField(u'CPU架构', max_length=64)
+    mem_total = models.IntegerField(u'内存(MB)')
+
+    os = models.CharField(u'系统类型', max_length=128)
+    os_release = models.CharField(u'系统版本', max_length=128)
+
+    hostname = models.CharField(u'主机名', max_length=128)
+    ip = models.GenericIPAddressField()
+    fqdn = models.CharField("FQDN", max_length=255)
     memo = models.TextField(u'备注', null=True, blank=True)
     created_at = models.DateTimeField(blank=True, auto_now_add=True)
     updated_at = models.DateTimeField(blank=True, auto_now=True)
@@ -18,4 +28,5 @@ class Host(models.Model):
         verbose_name_plural = u"主机"
 
     def __str__(self):
-        return '<type:%s id:%s>' % (self.type, self.id)
+        return '<%s ip:%s>' % (self.hostname, self.ip)
+
