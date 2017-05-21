edd064c9bdfd9203d762c3f3615da15b4b96235c
fangniu <838307912@qq.com>
2017-05-21 17:43:04 +0800

    add Home.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <home></home>
+    <!--<img src="./assets/logo.png">-->
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import Home from './components/Home.vue'
+  export default {
+    name: 'app',
+    components: {
+        Home
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*#app {*/
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+/*}*/
 </style>
