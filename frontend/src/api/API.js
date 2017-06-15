a5038eaa654ed52b366eb9dda10943e0acaf61fc
k2sheng <chensheng@k2data.com.cn>
2017-06-15 18:43:00 +0800

    add API

---
+++ frontend/src/api/API.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import config from './config'
+
+class API {
+  get (param) {
+    config.url = param.url;
+    config.method = 'GET';
+    return axios(config);
+  }
+  post (param) {
+    config.url = param.url;
+    config.data = param.data;
+    config.method = 'POST';
+    return axios(config);
+  }
+  reqSuccess(obj,msg){
+    obj.$message({
+      message: msg,
+      type: 'success'
+    });
+  }
+  reqFail(obj,msg){
+    obj.$message({
+      message: msg,
+      type: 'success'
+    });
+  }
+}
+export default API;
--- frontend/src/components/ServiceList.vue
+++ frontend/src/components/ServiceList.vue
@@ -23,22 +23,14 @@
         </el-col>
 
         <!--列表-->
-        <el-table :data="users" highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;">
+        <el-table :data="stack_list" highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;">
           <el-table-column type="selection" width="55">
           </el-table-column>
-          <el-table-column type="index" width="60">
+          <el-table-column prop="name" label="服务栈" span="4" sortable>
           </el-table-column>
-          <el-table-column prop="name" label="栈名" width="120" sortable>
+          <el-table-column prop="services" label="服务" span="14" sortable>
           </el-table-column>
-          <el-table-column prop="sex" label="性别" width="100" :formatter="formatSex" sortable>
-          </el-table-column>
-          <el-table-column prop="age" label="年龄" width="100" sortable>
-          </el-table-column>
-          <el-table-column prop="birth" label="生日" width="120" sortable>
-          </el-table-column>
-          <el-table-column prop="addr" label="地址" min-width="180" sortable>
-          </el-table-column>
-          <el-table-column label="操作" width="150">
+          <el-table-column label="操作" span="6">
             <template scope="scope">
               <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
               <el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>
@@ -55,24 +47,15 @@
 
         <!--编辑界面-->
         <el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
-          <el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
-            <el-form-item label="栈名" prop="name">
+          <el-form :model="editForm" label-width="80px" ref="editForm">
+            <el-form-item label="服务栈" prop="name">
               <el-input v-model="editForm.name" auto-complete="off"></el-input>
             </el-form-item>
-            <el-form-item label="性别">
-              <el-radio-group v-model="editForm.sex">
-                <el-radio class="radio" :label="1">男</el-radio>
-                <el-radio class="radio" :label="0">女</el-radio>
-              </el-radio-group>
+            <el-form-item label="ID号" prop="id">
+              <el-input v-model="editForm.id"></el-input>
             </el-form-item>
-            <el-form-item label="年龄">
-              <el-input-number v-model="editForm.age" :min="0" :max="200"></el-input-number>
-            </el-form-item>
-            <el-form-item label="生日">
-              <el-date-picker type="date" placeholder="选择日期" v-model="editForm.birth"></el-date-picker>
-            </el-form-item>
-            <el-form-item label="地址">
-              <el-input type="textarea" v-model="editForm.addr"></el-input>
+            <el-form-item label="yaml">
+              <el-input type="textarea" v-model="editForm.yml" :autosize=true></el-input>
             </el-form-item>
           </el-form>
           <div slot="footer" class="dialog-footer">
@@ -116,19 +99,31 @@
 </template>
 
 <script>
-
+  import API from '../api/API'
+  const api = new API();
   export default {
+    beforeMount(){
+      let that = this;
+      let params = {
+        url:"/kmxes",
+      };
+      api.get(params)
+        .then(function(res){
+          console.log(JSON.parse(res.data.data).sets[0].rows);
+        })
+        .catch(function(err){
+          console.log('11');
+          api.reqFail(that,"获取列表失败请刷新");
+        });
+    },
     data() {
       return {
         filters: {
           name: ''
         },
-        users: [  {
-          id: 1,
-          username: 'admin',
-          password: '123456',
-          avatar: 'https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png',
-          name: '张某某'
+        stack_list: [  {
+          name: 'myWeb',
+          services: "web, redis, db"
         }],
         total: 0,
         page: 1,
@@ -137,19 +132,11 @@
 
         editFormVisible: false,//编辑界面是否显示
         editLoading: false,
-        editFormRules: {
-          name: [
-            { required: true, message: '请输入姓名', trigger: 'blur' }
-          ]
-        },
         //编辑界面数据
         editForm: {
           id: 0,
-          name: '',
-          sex: -1,
-          age: 0,
-          birth: '',
-          addr: ''
+          name: 'myWeb',
+          yml: "version: \"3\"\nservices:\n\n  redis:\n    image: redis:alpine\n    ports:\n      - \"6379\"\n    networks:\n      - frontend\n    deploy:\n      replicas: 2\n      update_config:\n        parallelism: 2\n        delay: 10s\n      restart_policy:\n        condition: on-failure\n  db:\n    image: postgres:9.4\n    volumes:\n      - db-data:/var/lib/postgresql/data\n    networks:\n      - backend\n    deploy:\n      placement:\n        constraints: [node.role == manager]\n  vote:\n    image: dockersamples/examplevotingapp_vote:before\n    ports:\n      - 5000:80\n    networks:\n      - frontend\n    depends_on:\n      - redis\n    deploy:\n      replicas: 2\n      update_config:\n        parallelism: 2\n      restart_policy:\n        condition: on-failure\n  result:\n    image: dockersamples/examplevotingapp_result:before\n    ports:\n      - 5001:80\n    networks:\n      - backend\n    depends_on:\n      - db\n    deploy:\n      replicas: 1\n      update_config:\n        parallelism: 2\n        delay: 10s\n      restart_policy:\n        condition: on-failure\n\n  worker:\n    image: dockersamples/examplevotingapp_worker\n    networks:\n      - frontend\n      - backend\n    deploy:\n      mode: replicated\n      replicas: 1\n      labels: [APP=VOTING]\n      restart_policy:\n        condition: on-failure\n        delay: 10s\n        max_attempts: 3\n        window: 120s\n      placement:\n        constraints: [node.role == manager]\n\n  visualizer:\n    image: manomarks/visualizer\n    ports:\n      - \"8080:8080\"\n    stop_grace_period: 1m30s\n    volumes:\n      - \"/var/run/docker.sock:/var/run/docker.sock\"\n    deploy:\n      placement:\n        constraints: [node.role == manager]\n\nnetworks:\n  frontend:\n  backend:\n\nvolumes:\n  db-data:\n"
         },
 
         addFormVisible: false,//新增界面是否显示
@@ -228,7 +215,8 @@
       //显示编辑界面
       handleEdit: function (index, row) {
         this.editFormVisible = true;
-        this.editForm = Object.assign({}, row);
+        console.log(this.editForm)
+//        this.editForm = Object.assign({}, row);
       },
       //显示新增界面
       handleAdd: function () {
