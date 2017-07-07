afb6fa409809002908be36e4c331371dd4b60124
fangniu <838307912@qq.com>
2017-07-07 16:54:21 +0800

    update snapshot

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -2,7 +2,7 @@
   <div class="sidebar">
     <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
       <el-submenu index="1">
-        <template slot="title"><i class="el-icon-message"></i>导航一</template>
+        <template slot="title"><i class="el-icon-message"></i>仪表盘</template>
         <el-menu-item-group>
           <template slot="title">分组一</template>
           <el-menu-item index="1-1">选项1</el-menu-item>
@@ -16,8 +16,8 @@
           <el-menu-item index="1-4-1">选项1</el-menu-item>
         </el-submenu>
       </el-submenu>
-      <el-menu-item index="2"><i class="el-icon-menu"></i>导航二</el-menu-item>
-      <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
+      <el-menu-item index="2"><i class="el-icon-menu"></i>Docker管理</el-menu-item>
+      <el-menu-item index="3"><i class="el-icon-setting"></i>主机管理</el-menu-item>
     </el-menu>
   </div>
 </template>
